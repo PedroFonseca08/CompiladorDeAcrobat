e91e12183a3d9474899eb32587f3aba5727969e5
ThiagoHenrique <thiago-maglhaes@hotmail.com>
2023-07-28 14:03:32 -0300

    acabou mau mau

--- CompiladorDeAcrobat/src/ExemploLexer.java
+++ CompiladorDeAcrobat/src/ExemploLexer.java
@@ -19,9 +19,13 @@ public class ExemploLexer {
             Token token;
             while (!lexer._hitEOF){
                 token = lexer.nextToken();
-                System.out.println("Token: "+token.toString().replace("%", "%%"));
-                System.out.printf("\t\tLexema: "+ token.getText().replace("%", "%%"));
-                System.out.println("\t\tClasse: "+lexer.getVocabulary().getDisplayName(token.getType()).replace("%", "%%"));
+                if(lexer.getVocabulary().getDisplayName(token.getType()).replace("%", "%%").equals("ErrorChar")){
+                    throw new RuntimeException("ERRO AO COMPILAR! Foi registrado um erro na analise lexica.");
+                }else{
+                    System.out.println("Token: "+token.toString().replace("%", "%%"));
+                    System.out.printf("\t\tLexema: "+ token.getText().replace("%", "%%"));
+                    System.out.println("\t\tClasse: "+lexer.getVocabulary().getDisplayName(token.getType()).replace("%", "%%"));
+                }
             }
         }catch(IOException e){
             e.printStackTrace();
@@ -69,7 +73,7 @@ public class ExemploLexer {
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(listener,ast);
         if ( listener.getErro() ){
-            throw new RuntimeException("ERRO AO COMPILAR!");
+            throw new RuntimeException("ERRO AO COMPILAR! Foi registrado um erro semantico.");
         }
         else {
             System.out.println("Tabela: " + listener.getTabelaSimbolos().toString());
@@ -81,13 +85,13 @@ public class ExemploLexer {
         System.out.println("Pressione '1' para ver saida analisador lexico");
         System.out.println("Pressione '2' para ver saida analisador sintatico");
         System.out.println("Pressione '3' para ver saida analisador semantico");
-        System.out.println("Pressione '0' para ver sair");
+        System.out.println("Pressione '0' para sair");
     }
 
     public static void main (String[] args){
         menu();
         int opcao = Integer.parseInt(entrada.nextLine());
-        String filename = "C:\\Users\\User\\Documents\\Aulas\\PROGRAMAÇÃO\\COMPUTADOR\\COMPILADORES\\EXERCICIOS\\CompiladorDeAcrobat\\CompiladorDeAcrobat\\semanticaTestes.txt";
+        String filename = "C:\\Users\\User\\Documents\\Aulas\\PROGRAMAÇÃO\\COMPUTADOR\\COMPILADORES\\EXERCICIOS\\CompiladorDeAcrobat\\CompiladorDeAcrobat\\codigoFibo.txt";
 
         while(opcao != 0){
             if(opcao == 1){
